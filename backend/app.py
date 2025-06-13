85929d3d934753f90d6a65aed66cf25f5badfec7
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-06-13 12:05:21 +0800

    Fix flake8 linting errors

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/api/calculate")
 def calculate():
@@ -8,6 +10,7 @@ def calculate():
     num2 = request.args.get("num2", type=int)
     result = num1 + num2
     return jsonify(result=result)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
